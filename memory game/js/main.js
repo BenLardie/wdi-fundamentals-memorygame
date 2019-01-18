17539d985b2d807c72732603dd8637b794e07535
Benjamin Lardie <ben@Benjamins-MacBook-Air.local>
2019-01-17 22:58:52 -0500

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -1,7 +1,15 @@
-console.log('Up and running!');
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardTwo);
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+console.log('You Flipped' + cardOne);
+console.log('You Fliiped' + cardTwo);
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match!");
+} else { alert("Sorry, try again")
+};
